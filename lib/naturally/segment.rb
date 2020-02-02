a99b89ccfa84bf175cf9c3492e0a9031c78c03d0
Reuben Brown <reubendbrown@gmail.com>
2020-02-02 09:52:37 -0600

    Convert all roman numerals under 100 into integers so they can be sorted appropriately

--- lib/naturally/segment.rb
+++ lib/naturally/segment.rb
@@ -36,8 +36,38 @@ module Naturally
         [:str, $1, $2.to_i]
       elsif @val =~ /^\p{Digit}+$/
         [:int, @val.to_i]
+      elsif @val.upcase =~ /^(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+        [:int, convert_roman_numerals]
       else
         [:str, @val]
+      end
+    end
+
+
+    # Convert the given roman numeral string (between 0-100)
+    # into an integer so objects with roman numerals can
+    # accurately be compared against each other.
+    #
+    # @return [Integer] a representation of the roman numeral
+    #         as an integer
+    def convert_roman_numerals
+      @val =
+        if @val.include?('iv')
+          @val.gsub('iv', '4')
+        elsif @val.include?('ix')
+          @val.gsub('ix', '9')
+        elsif @val.include?('xl')
+          @val.gsub('xl', 'f')
+        elsif @val.include?('xc')
+          @val.gsub('xc', 'n')
+        else
+          @val
+        end
+
+      hashmap = { 'i'=>1,'4'=>4,'v'=>5,'9'=>9,'x'=>10,'f'=>40,'l'=>50,'n'=>90,'c'=>100 }
+
+      @val = @val.chars.collect do |numeral|
+        hashmap[numeral]
       end
     end
   end
